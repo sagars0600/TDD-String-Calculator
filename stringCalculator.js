12faa838af5ce4466c2157e701874faa0bc478bd
sagars0600 <sagarsolanki2120@gmail.com>
2025-02-11 22:33:38 +0530

    Test 8: Support delimiters of any length

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,14 +4,15 @@ function add(numbers) {
   let delimiter = ",";
   if (numbers.startsWith("//")) {
     const parts = numbers.split("\n");
-    delimiter = parts[0].substring(2);
+    const delimiterMatch = parts[0].match(/\[(.*?)\]/);
+    delimiter = delimiterMatch ? delimiterMatch[1] : parts[0].substring(2);
     numbers = parts[1];
   }
 
   const numArray = numbers
     .split(new RegExp(`[${delimiter}\n]`))
     .map(Number)
-    .filter((num) => num <= 1000); // Ignore numbers bigger than 1000
+    .filter((num) => num <= 1000);
 
   const negativeNumbers = numArray.filter((num) => num < 0);
   if (negativeNumbers.length > 0) {
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -38,8 +38,23 @@ test("should return the number itself when a single number is provided", () => {
   expect(result).toBe(expected);
 });
 
-
 //test 3
+test("should return the sum of two comma-separated numbers", () => {
+  const input = "1,2";
+  const expected = 3;
+  const result = add(input);
+
+  logTestResult(
+    "should return the sum of two comma-separated numbers",
+    input,
+    expected,
+    result
+  );
+
+  expect(result).toBe(expected);
+});
+
+//test4
 test("should return the sum when numbers are separated by commas and new lines", () => {
   const input = "1\n2,3";
   const expected = 6;
@@ -55,7 +70,7 @@ test("should return the sum when numbers are separated by commas and new lines",
   expect(result).toBe(expected);
 });
 
-//test 4
+//test 5
 
 test("should support custom delimiters specified at the beginning of the string", () => {
   const input = "//;\n1;2";
@@ -72,13 +87,13 @@ test("should support custom delimiters specified at the beginning of the string"
   expect(result).toBe(expected);
 });
 
-//test 5
+//test 6
 test("should throw an exception for negative numbers with the message 'negative numbers not allowed'", () => {
   const input = "-1,2,-3";
   expect(() => add(input)).toThrow("negative numbers not allowed: -1, -3");
 });
 
-//test 6
+//test 7
 test("should ignore numbers bigger than 1000", () => {
   const input = "2,1001";
   const expected = 2;
@@ -86,6 +101,22 @@ test("should ignore numbers bigger than 1000", () => {
 
   logTestResult(
     "should ignore numbers bigger than 1000",
+    input,
+    expected,
+    result
+  );
+
+  expect(result).toBe(expected);
+});
+
+//test 8
+test("should support delimiters of any length", () => {
+  const input = "//[***]\n1***2***3";
+  const expected = 6;
+  const result = add(input);
+
+  logTestResult(
+    "should support delimiters of any length",
     input,
     expected,
     result
